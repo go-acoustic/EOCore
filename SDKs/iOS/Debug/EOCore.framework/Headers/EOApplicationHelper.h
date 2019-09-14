4c0cf3b1ed45dbb4078439d006f7aa9489073d6e
Omar Hernandez <ohernandez@us.ibm.com>
2019-09-13 18:25:26 -0700

    EOCore beta build: 2.1.56
    Beta Change Notes:
    Enhancements
    
    - Add networkCarrierNames for modules.
    - Add ipv6 for modules.
    
    Bug Fixes
    
    - Fixed an issue with cocoapods using two dot natation versus three dot notation.
    
    Known Issues
    
    - Support for arm64e will be added soon, Apple has not completed the specification. Currently it is just beta.

--- SDKs/iOS/Debug/EOCore.framework/Headers/EOApplicationHelper.h
+++ SDKs/iOS/Debug/EOCore.framework/Headers/EOApplicationHelper.h
@@ -1,14 +1,17 @@
 //
-//  Licensed Materials - Property of IBM
-//  (C) Copyright IBM Corp. 2019
-//  US Government Users Restricted Rights - Use, duplication or disclosure
-//  restricted by GSA ADP Schedule Contract with IBM Corp.
+// Copyright (C) 2019 Acoustic, L.P. All rights reserved.
+//
+// NOTICE: This file contains material that is confidential and proprietary to
+// Acoustic, L.P. and/or other developers. No license is granted under any intellectual or
+// industrial property rights of Acoustic, L.P. except as may be provided in an agreement with
+// Acoustic, L.P. Any unauthorized copying or distribution of content from this file is
+// prohibited.
 //
 
 /*!
  @file EOApplicationHelper.h
  @brief Header file defining all manual API calls that can be made to the TeaLeaf library
- @copyright IBM Corp. 2019
+ @copyright Acoustic, L.P. 2019
  */
 #import <Foundation/Foundation.h>
 #import <EOCore/NSString+EOCoreExtensions.h>
