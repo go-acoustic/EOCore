c135b699ada45ff96ff3b672b053b551e829e134
BenderRodriguez <experienceanalytics.sdk@acoustic.co>
2021-09-13 21:08:17 -0700

    Beta EOCore build: 2_3_60
    Beta EOCore Change Notes:
    Enhancements
    
    - Library is now being built for xcframeworks with latest xcode 12.5.
    
    Bug Fixes
    
    - Added extra checks and synchronized for queue that is posting back to server during application going to background or to terminate. There was some crashes reported, but unable to reproduce locally.
    
    Known Issues
    
    - After iOS 13, library no longer supports iPad because of Apple changes in iPad OS.
    - Dual sim features are in beta. Example information using multiple carriers.

--- SDKs/iOS/Debug/EOCore.xcframework/ios-arm64_i386_x86_64-simulator/EOCore.framework/Headers/EOPublicHeader.h
+++ SDKs/iOS/Debug/EOCore.xcframework/ios-arm64_i386_x86_64-simulator/EOCore.framework/Headers/EOPublicHeader.h
@@ -11,7 +11,7 @@
 // prohibited.
 //
 
-#define EOCORE_FRAMEWORK_VER @"2.3.59"
+#define EOCORE_FRAMEWORK_VER @"2.3.60"
 
 // Constants Config Items
 #define kEOConfig_CriticalBatteryLevel @"CriticalBatteryLevel"
