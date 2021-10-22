7c3bfbed191f3041e836a816e7aca8ed1cb2ef4c
BenderRodriguez <experienceanalytics.sdk@acoustic.co>
2021-10-21 21:39:05 -0700

    October 21 2021 EOCore Module 2.3.68 EOCore Release
    October 21 2021 EOCore Module 2.3.68
    EOCore Release
    Enhancements
    
    - Library is now being built for xcframeworks with latest xcode 13.0.
    
    Bug Fixes
    
    - Added extra checks and synchronized for queue that is posting back to server during application going to background or to terminate. There was some crashes reported, but unable to reproduce locally.
    
    Known Issues
    
    - After iOS 13, library no longer supports iPad because of Apple changes in iPad OS.
    - Dual sim features are in beta. Example information using multiple carriers.

--- SDKs/iOS/Release/EOCore.xcframework/ios-arm64_armv7/EOCore.framework/Headers/EOPublicHeader.h
+++ SDKs/iOS/Release/EOCore.xcframework/ios-arm64_armv7/EOCore.framework/Headers/EOPublicHeader.h
@@ -11,7 +11,7 @@
 // prohibited.
 //
 
-#define EOCORE_FRAMEWORK_VER @"2.3.67"
+#define EOCORE_FRAMEWORK_VER @"2.3.68"
 
 // Constants Config Items
 #define kEOConfig_CriticalBatteryLevel @"CriticalBatteryLevel"
