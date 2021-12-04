9e27145a498e4973f49857f9680d17d7cca5bcfd
BenderRodriguez <experienceanalytics.sdk@acoustic.co>
2021-12-04 03:51:07 -0800

    Beta EOCore build: 2.3.110
    Beta EOCore Change Notes:
    Enhancements
    
    - None.
    
    Bug Fixes
    
    - None.
    
    Known Issues
    
    - After iOS 13, library no longer supports iPad because of Apple changes in iPad OS.
    - Dual sim features are in beta. Example information using multiple carriers.

--- SDKs/iOS/Debug/EOCore.xcframework/ios-arm64_i386_x86_64-simulator/EOCore.framework/Headers/EOPublicHeader.h
+++ SDKs/iOS/Debug/EOCore.xcframework/ios-arm64_i386_x86_64-simulator/EOCore.framework/Headers/EOPublicHeader.h
@@ -11,7 +11,7 @@
 // prohibited.
 //
 
-#define EOCORE_FRAMEWORK_VER @"2.3.109"
+#define EOCORE_FRAMEWORK_VER @"2.3.110"
 
 // Constants Config Items
 #define kEOConfig_CriticalBatteryLevel @"CriticalBatteryLevel"
