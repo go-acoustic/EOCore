2369d25bffa2edaa55592d81f6236b9199c2f3cf
BenderRodriguez <experienceanalytics.sdk@acoustic.co>
2022-06-10 11:34:04 -0700

    Beta EOCore build: 2.3.231
    Beta EOCore Change Notes:
    Bug Fixes
    
    - Update syntax due to swift issue with NSNumber during json creation.
    
    Improvements:
    
    - New minimal os is 12.0 from 9.0 for library.
    - Add more debug information what is being used for configuration settings.
    
    Our environment for this release:
    
    - Xcode 13.2.1
    - MacOS 11.6.1
    - iOS 12.x to 15.x
    - Supported architectures:
    	- simulator
    		- arm64
    		- x86_64
    	- device
    		- arm64
    
    Known Issues
    
    - After iOS 13, library no longer supports iPad because of Apple changes in iPad OS.
    - Dual sim features are in beta. Example information using multiple carriers.

--- SDKs/iOS/Debug/EOCore.xcframework/ios-arm64_i386_x86_64-simulator/EOCore.framework/Headers/EOPublicHeader.h
+++ SDKs/iOS/Debug/EOCore.xcframework/ios-arm64_i386_x86_64-simulator/EOCore.framework/Headers/EOPublicHeader.h
@@ -11,7 +11,7 @@
 // prohibited.
 //
 
-#define EOCORE_FRAMEWORK_VER @"2.3.230"
+#define EOCORE_FRAMEWORK_VER @"2.3.231"
 
 // Constants Config Items
 #define kEOConfig_CriticalBatteryLevel @"CriticalBatteryLevel"
