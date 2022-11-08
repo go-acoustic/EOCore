f0d23d24c32d32c89c76a136aa4147197aca4d65
BenderRodriguez <experienceanalytics.sdk@acoustic.co>
2022-11-07 21:07:41 -0800

    Beta EOCore build: 2.3.254
    Beta EOCore Change Notes:
    Bug Fixes
    
    - None.
    
    Improvements:
    
    - Add support for remote configuration.
    - Add support to report 5G connection.
    - Updated to new Apple xcode 14 standards(https://developer.apple.com/documentation/xcode-release-notes/xcode-14-release-notes).
    	- No more bitcode, because bitcode is now deprecated, builds for iOS, tvOS, and watchOS no longer include bitcode by default.
    	- Only arm64 & x86_64, building iOS projects with deployment targets for the armv7, armv7s, and i386 architectures is no longer supported.
    	- Building for deployment to OS releases older than macOS 10.13, iOS 11, tvOS 11, and watchOS 4 is no longer supported.
    
    Our environment for this release:
    
    - Xcode 14.0.1
    - MacOS 12.6
    - iOS 12.x to 16.x
    - Supported architectures:
    	- simulator
    		- arm64
    		- x86_64
    	- device
    		- arm64
    
    Known Issues
    
    - After iOS 13, library no longer supports iPad because of Apple changes in iPad OS.
    - Dual sim features are in beta. Example information using multiple carriers.

--- SDKs/iOS/Debug/EOCore.xcframework/ios-arm64/EOCore.framework/Headers/EOPublicHeader.h
+++ SDKs/iOS/Debug/EOCore.xcframework/ios-arm64/EOCore.framework/Headers/EOPublicHeader.h
@@ -11,7 +11,7 @@
 // prohibited.
 //
 
-#define EOCORE_FRAMEWORK_VER @"2.3.253"
+#define EOCORE_FRAMEWORK_VER @"2.3.254"
 
 // Constants Config Items
 #define kEOConfig_CriticalBatteryLevel @"CriticalBatteryLevel"
