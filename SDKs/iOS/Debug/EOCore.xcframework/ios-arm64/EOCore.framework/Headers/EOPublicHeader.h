5410a70e61be6c5fad2d985765e9dda3d6945e3b
BenderRodriguez <experienceanalytics.sdk@acoustic.co>
2022-11-28 18:18:20 -0800

    Beta EOCore build: 2.3.256
    Beta EOCore Change Notes:
    Bug Fixes
    
    - None.
    
    Improvements:
    
    - Add ability to post data during a fatalError in Swift via synchronous post.
    
    Our environment for this release:
    
    - Xcode 14.1
    - MacOS 13.0.1
    - iOS 12.x to 16.x
    - Supported architectures:
    	- simulator
    		- arm64
    		- x86_64
    	- device
    		- arm64
    
    Known Issues
    
    - After iOS 13, library no longer supports iPad because of Apple changes in iPad OS.
    - Dual sim features are in beta. Example information using multiple carriers.

--- SDKs/iOS/Debug/EOCore.xcframework/ios-arm64/EOCore.framework/Headers/EOPublicHeader.h
+++ SDKs/iOS/Debug/EOCore.xcframework/ios-arm64/EOCore.framework/Headers/EOPublicHeader.h
@@ -11,7 +11,7 @@
 // prohibited.
 //
 
-#define EOCORE_FRAMEWORK_VER @"2.3.255"
+#define EOCORE_FRAMEWORK_VER @"2.3.256"
 
 // Constants Config Items
 #define kEOConfig_CriticalBatteryLevel @"CriticalBatteryLevel"
