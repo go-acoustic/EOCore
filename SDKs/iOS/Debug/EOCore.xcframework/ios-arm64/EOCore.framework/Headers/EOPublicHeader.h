f64ca2eaf9da0d87e9f390cae9cd5bd464c4272f
BenderRodriguez <experienceanalytics.sdk@acoustic.co>
2025-01-17 18:19:59 -0800

    Beta EOCore build: 2.3.299
    Beta EOCore Change Notes:
    Bug Fixes:
    
    - Fix issue with xcframework signature to fix issue with validation.
    - Fix issue with Swift Package checksum.
    
    Improvements:
    
    - None.
    
    Our environment for this release:
    
    - Xcode 16.0
    - MacOS 15.0
    - iOS 13.x to 18.x
    - Supported architectures:
    	- simulator
    		- arm64
    		- x86_64
    	- device
    		- arm64
    
    Known Issues
    
    - After iOS 13, library no longer supports iPad because of Apple changes in iPad OS.
    - Dual sim features are in beta. Example information using multiple carriers.

--- SDKs/iOS/Debug/EOCore.xcframework/ios-arm64/EOCore.framework/Headers/EOPublicHeader.h
+++ SDKs/iOS/Debug/EOCore.xcframework/ios-arm64/EOCore.framework/Headers/EOPublicHeader.h
@@ -11,7 +11,7 @@
 // prohibited.
 //
 
-#define EOCORE_FRAMEWORK_VER @"2.3.298"
+#define EOCORE_FRAMEWORK_VER @"2.3.299"
 
 // Constants Config Items
 #define kEOConfig_CriticalBatteryLevel @"CriticalBatteryLevel"
