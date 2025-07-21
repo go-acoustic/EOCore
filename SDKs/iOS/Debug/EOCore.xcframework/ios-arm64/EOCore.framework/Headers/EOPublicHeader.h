93f3fcbbbd99d139a842b155375748df5826ad65
BenderRodriguez <experienceanalytics.sdk@acoustic.co>
2025-07-21 12:24:52 -0700

    Beta EOCore build: 2.3.320
    Beta EOCore Change Notes:
    Bug Fixes:
    
    - None.
    
    Improvements:
    
    - Adjust ios library to support react native for npm plugin.
    - Adjust ios library to new minimal of 15.1.
    
    Our environment for this release:
    
    - Xcode 16.4
    - MacOS 15.5
    - iOS 15.1 to 18.x
    - Supported architectures:
    	- simulator
    		- arm64
    		- x86_64
    	- device
    		- arm64
    
    Known Issues
    
    - After iOS 13, library no longer supports iPad because of Apple changes in iPad OS.
    - Dual sim features are in beta. Example information using multiple carriers.

--- SDKs/iOS/Debug/EOCore.xcframework/ios-arm64/EOCore.framework/Headers/EOPublicHeader.h
+++ SDKs/iOS/Debug/EOCore.xcframework/ios-arm64/EOCore.framework/Headers/EOPublicHeader.h
@@ -11,7 +11,7 @@
 // prohibited.
 //
 
-#define EOCORE_FRAMEWORK_VER @"2.3.319"
+#define EOCORE_FRAMEWORK_VER @"2.3.320"
 
 // Constants Config Items
 #define kEOConfig_CriticalBatteryLevel @"CriticalBatteryLevel"
