58211b353ceea582d9dae4095d772b48756a2605
BenderRodriguez <experienceanalytics.sdk@acoustic.co>
2026-03-12 09:47:03 -0700

    Beta EOCore build: 2.3.333
    Beta EOCore Change Notes:
    Bug Fixes:
    
    - Default to system value for User-Agent header if it is not available
    
    Improvements:
    
    - None.
    
    Our environment for this release:
    
    - Xcode 26.0.1
    - MacOS 15.6.1
    - iOS 15.1 to 26.x
    - Supported architectures:
    	- simulator
    		- arm64
    		- x86_64
    	- device
    		- arm64
    
    Known Issues
    
    - After iOS 13, library no longer supports iPad because of Apple changes in iPad OS.
    - Dual sim features are in beta. Example information using multiple carriers.

--- SDKs/iOS/Debug/EOCore.xcframework/ios-arm64/EOCore.framework/Headers/EOPublicHeader.h
+++ SDKs/iOS/Debug/EOCore.xcframework/ios-arm64/EOCore.framework/Headers/EOPublicHeader.h
@@ -11,7 +11,7 @@
 // prohibited.
 //
 
-#define EOCORE_FRAMEWORK_VER @"2.3.332"
+#define EOCORE_FRAMEWORK_VER @"2.3.333"
 
 // Constants Config Items
 #define kEOConfig_CriticalBatteryLevel @"CriticalBatteryLevel"
